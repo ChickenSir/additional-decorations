cfb0c3db470c751f69e09d08c8da3d28b3c229dd
ChickenSir <shyguyman11@gmail.com>
2024-02-10 20:33:30 +1100

    Update Fabric Block Settings for iron ingot stack

--- src/main/java/com/additionaldecorations/Collections/BlocksCollection.java
+++ src/main/java/com/additionaldecorations/Collections/BlocksCollection.java
@@ -5,13 +5,14 @@ import com.additionaldecorations.Blocks.IronIngotStack;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class BlocksCollection {
-    public static final Block IRON_INGOT_STACK = new IronIngotStack(FabricBlockSettings.create().strength(4.0f));
+    public static final Block IRON_INGOT_STACK = new IronIngotStack(FabricBlockSettings.copyOf(Blocks.IRON_BARS));
 
     public static void register(String modID) {
         // Register Blocks
